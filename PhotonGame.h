9fc1b1653dc48abbbc5036db17cc87bec4e84ba8
Hou Jianfeng <derekwangyi@163.com>
2016-06-17 12:17:56 +0800

    Add files via upload

---
+++ PhotonGame.h
@@ -0,0 +1,29 @@
+#pragma once
+
+//header files include
+#include "cocos2d.h"
+#include "SimpleAudioEngine.h"
+#include "cocostudio\CocoStudio.h"
+#include "ui\CocosGUI.h"
+
+//Macros defination
+//Namespace
+#define USING_NS_CC_DEN using namespace CocosDenshion
+
+//Constants
+#define PI 3.14159265
+#define RED Color3B(248, 68, 98)
+#define GREEN Color3B(168, 255, 100)
+#define BLUE Color3B(40, 180, 255)
+#define DARK Color3B(50, 50, 50)
+#define RED_GREEN Color3B(255, 255, 0)
+#define RED_BLUE Color3B(255, 0, 255)
+#define GREEN_BLUE Color3B(0, 255, 255)
+#define WINDOW_SIZE 960, 640
+#define MUSIC_KEY "music_key"
+#define SOUND_KEY "sound_key"
+#define MUSIC_PLAYED_KEY "music_played_key"
+
+//File path
+#define FILE_BGM "sound/Bgm_MoscowOutskirtsNight.mp3"
+#define FILE_SOUND_1 "sound/Menu_click.wav"
