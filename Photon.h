89f951d70ecc08e84f60b0b2416299afc16329fa
Hou Jianfeng <derekwangyi@163.com>
2016-06-17 21:33:43 +0800

    Update Photon.h

--- Photon.h
+++ Photon.h
@@ -2,17 +2,23 @@
 #define _PHOTON_H_
 
 #include "cocos2d.h"
+#include "Lense.h"
 #include <vector>
 
 class Photon : public cocos2d::Sprite
 {
 public:
+	static std::vector<Photon*> photons;
 	virtual bool init();
 	void setVelocity(cocos2d::Vec2 v);
 	cocos2d::Vec2 getVelocity();
+	void setAccl(cocos2d::Vec2 a);
+	cocos2d::Vec2 getAccl();
+	bool meetLense(Lense* lense);
 	CREATE_FUNC(Photon);
 private:
 	cocos2d::Vec2 velocity;
+	cocos2d::Vec2 accl;
 };
 
 #endif // _PHOTON_H_
