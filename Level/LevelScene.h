1483d78d6cfd40032bd097d1e7ebd63819b167e7
Hou Jianfeng <derekwangyi@163.com>
2016-06-20 01:38:31 +0800

    Update LevelScene.h

--- Level/LevelScene.h
+++ Level/LevelScene.h
@@ -2,6 +2,8 @@
 #define _LEVEL_SCENE_H_
 
 #include "PhotonGame.h"
+#include "Photon.h"
+#include "Lense.h"
 #include <vector>
 
 class Level : public cocos2d::Layer {
@@ -13,18 +15,40 @@ protected:
 	cocos2d::ui::Button *nextLevelButton;
 
 public:
+	std::vector<Photon*> photons;
+	std::vector<Lense*> lenses;
+	std::vector<cocos2d::Sprite*> movableLenses;
+	std::vector<cocos2d::Sprite*> rotatableLenses;
+	cocos2d::Sprite* rotatingLense = nullptr;
+	cocos2d::Sprite* movingLense = nullptr;
+
 	static cocos2d::Scene* Level::createScene();
 	static cocos2d::Scene* createLevel(int levelNum);
 
 	virtual bool init();
 
 	cocos2d::Label* addLevelTitle();
-	cocos2d::Sprite* addLense(std::string fileName, cocos2d::Vec2 pos, double angle = 0.0);
-	cocos2d::Sprite* addReceiver(std::string fileName, cocos2d::Vec2 pos, cocos2d::Color3B color = DARK);
+	cocos2d::Sprite* addLightSource(cocos2d::Vec2 pos, cocos2d::Color3B color = WHITE);
+	cocos2d::Sprite* addReceiver(cocos2d::Vec2 pos, cocos2d::Color3B color = DARK);
+	
+	template<typename T>
+	T* addLense(const std::string &fileName, cocos2d::Vec2 pos, double angle = 0.0) {
+		T* newLense = Lense::create<T>(fileName, pos, angle);
+		this->addChild(newLense);
+		lenses.push_back(newLense);
+
+		return newLense;
+	}
 
 	void initNextLevelButton();
 	void nextLevelButtonOnUse(bool statu);
 	int getLevelNumber() const { return levelNumber; }
+
+	bool touchBegan(cocos2d::Touch* touch, cocos2d::Event* event);
+	void touchMoved(cocos2d::Touch* touch, cocos2d::Event* event);
+
+	virtual void onEnter();
+	virtual void onExit();
 
 	virtual void onEnterTransitionDidFinish();
 
